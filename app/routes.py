5976734a956558323f0490c1297afc398a407705
Amit Asghar <amitasghar@yahoo.com>
2019-08-22 23:02:19 -0400

    added error handling for page not found

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "hello world"
+@app.route('/hello')
+def hello():
+  return 'hello world'
+
+#echo a word
+@app.route('/echo/<word>')
+def show_user(word):  
+  return 'Word is: %s' % word
