8ca79c6d965108766a0f2ccb8960de71c59dd48e
amitasghar <aasghar@loyalty.com>
2019-08-28 16:28:32 -0400

    -added db -added read all messages endpoint -todo: expose endpoint from front-end (call on load)

--- forum.py
+++ forum.py
@@ -1,5 +1,21 @@
 # 3rd party modules
 from flask import make_response, abort
+from config import db
+from models import Message, MessageSchema
+
+def read_all():
+    """
+    This function responds to a request for /api/message
+    with the complete lists of messages
+    :return:        json string of list of messages
+    """
+    # Create the list of people from our data
+    message = Message.query.order_by(Message.name).all()
+
+    # Serialize the data for the response
+    message_schema = MessageSchema(many=True)
+    data = message_schema.dump(message).data
+    return data
 
 def echo(word):
     return {'msg': word}, 200    
---
+++ models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from config import db, ma
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    message_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True)
+    text_entry = db.Column(db.String(100), index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class MessageSchema(ma.ModelSchema):
+    class Meta:
+        model = Message
+        sqla_session = db.session    
