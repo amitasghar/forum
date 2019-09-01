c7c5d0342abbcf0a80cd5bb2847617a55ed1dc7f
amitasghar <aasghar@loyalty.com>
2019-09-01 01:06:07 -0400

    - updated test cases

--- runtests.bat
+++ runtests.bat
@@ -1,5 +1,5 @@
-echo "Run Unit Tests - ytest"
-pytest
+REM "Run Unit Tests - pytest"
+REM pytest - removed as we are using ORM and cypress does a better job
 echo "Run Integration Tests - Cypress"
 npx cypress run
 echo "API Testing - Postman tests need to be run manually"
