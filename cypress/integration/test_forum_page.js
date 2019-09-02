42d60863930f0360502dcb16028f0cc55bef70bf
amitasghar <aasghar@loyalty.com>
2019-09-02 19:18:26 -0400

    test cases updated

--- cypress/integration/test_forum_page.js
+++ cypress/integration/test_forum_page.js
@@ -1,19 +1,23 @@
+var delay = 500
+
 describe('test forum webpage', function () {
   it('enter word and verify word is output to a table', function () {
     cy.visit('http://localhost:5000')
     cy.get('#name').type("tester")
     var randomNum = Math.floor(Math.random() * 1000000)
     cy.get('#post').type("abcd" + randomNum + "#$%!")
+    cy.wait(delay)
 
     cy.get('#create').click()
+    cy.wait(delay)
 
-    cy.get('table').contains('td', "abcd" + randomNum + "#$%!");
+    cy.get('.message > table').contains('td', "abcd" + randomNum + "#$%!");
   })
 
   it('press post button with no message text', function () {
     cy.visit('http://localhost:5000')
     cy.get('#name').type("abcd")
-
+    cy.wait(delay)
     const stub = cy.stub()
     cy.on('window:alert', stub)
     cy.contains('Post').click()
@@ -25,7 +29,7 @@ describe('test forum webpage', function () {
   it('press post button with no username', function () {
     cy.visit('http://localhost:5000')
     cy.get('#post').type("abcd")
-
+    cy.wait(delay)
     const stub = cy.stub()
     cy.on('window:alert', stub)
     cy.contains('Post').click()
@@ -36,7 +40,7 @@ describe('test forum webpage', function () {
 
   it('press post button with no username and text', function () {
     cy.visit('http://localhost:5000')
-
+    cy.wait(delay)
     const stub = cy.stub()
     cy.on('window:alert', stub)
     cy.contains('Post').click()
@@ -44,4 +48,30 @@ describe('test forum webpage', function () {
         expect(stub.getCall(0)).to.be.calledWith('Problem with message post input')
       })
   })
+
+  it('enter user and check if user messages are displayed', function () {
+    cy.visit('http://localhost:5000')
+    var randomNum = Math.floor(Math.random() * 1000000)
+    cy.get('#name').type("tester")
+    cy.wait(delay)
+    cy.get('#post').type("abcd" + randomNum + "#$%!")
+    cy.wait(delay)
+    cy.get('#create').click()
+    cy.wait(delay)
+    cy.get('#name').type("tester")
+    cy.wait(delay)
+    cy.get('#post').type("efgh" + randomNum + "#$%!")
+    cy.wait(delay)
+    cy.get('#create').click()
+    cy.wait(delay)
+
+    cy.get('#findname').type("tester")
+    cy.wait(delay)
+    cy.get('#find').click()
+    cy.wait(delay)
+
+    cy.get('.message_by_user > table').contains('td', "abcd" + randomNum + "#$%!");
+    cy.get('.message_by_user > table').contains('td', "efgh" + randomNum + "#$%!");
+  })
+
 })
